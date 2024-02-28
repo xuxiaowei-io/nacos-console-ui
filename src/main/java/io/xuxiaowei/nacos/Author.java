dd61c58732f2953177f2847fa2077c912a7772ec
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-02-28 15:18:06 +0800

    :sparkles: 发布 javadoc

---
+++ src/main/java/io/xuxiaowei/nacos/Author.java
@@ -0,0 +1,9 @@
+package io.xuxiaowei.nacos;
+
+/**
+ * @author xuxiaowei
+ * @see <a href="https://github.com/xuxiaowei-com-cn/nacos-console-ui">nacos-console-ui</a>
+ */
+public class Author {
+
+}
